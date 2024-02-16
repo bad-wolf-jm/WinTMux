34b968d8632f35717ba4b355db46bfb37a8f95c5
Jean-Martin Albert <jm.albert@gmail.com>
2024-02-16 14:36:13 -0500

    Draw border around child terminals

--- Source/Launch/Main.cpp
+++ Source/Launch/Main.cpp
@@ -2,6 +2,7 @@
 #include "MainWindow/MainWindow.h"
 //#include "MainWindow/CommandLine.h"
 
+#define IMGUI_DEFINE_MATH_OPERATORS
 #include "imgui.h"
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
--- Source/Workspace/Terminal.cpp
+++ Source/Workspace/Terminal.cpp
@@ -1,8 +1,17 @@
 #include "Terminal.h"
 #include "imgui.h"
+#include "imgui_internal.h"
 
 void Terminal::Render()
 {
-    ImGui::SetCursorPos(ImVec2{5.0f, 5.0f});
+    auto  *drawList             = ImGui::GetWindowDrawList();
+    ImVec2 cursorScreenPosition = ImGui::GetCursorScreenPos();
+
+    ImGui::SetCursorPos( ImVec2{ 5.0f, 5.0f } );
     ImGui::Text( "FOOBAR" );
+
+    ImVec2 topLeft     = cursorScreenPosition;
+    ImVec2 size        = ImGui::GetWindowSize();
+    ImVec2 bottomRight = ImVec2{ topLeft.x + size.x, topLeft.y + size.y };
+    drawList->AddRect( topLeft, bottomRight, ImColor( 255, 0, 0, 255 ) );
 }
