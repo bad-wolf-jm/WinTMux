8efb21300ae9c7501acf54904387ad1ffbba49b5
Jean-Martin Albert <jm.albert@gmail.com>
2024-02-27 14:35:09 -0500

    DIsplay terminal text in inner child window

--- Source/Workspace/Workspace.cpp
+++ Source/Workspace/Workspace.cpp
@@ -1,5 +1,7 @@
 #include "Workspace.h"
+#include <iostream>
 #include <memory>
+#include <ostream>
 
 Workspace::Workspace( string_t name )
     : _name{ name }
@@ -29,23 +31,32 @@ void Workspace::Render()
     int32_t terminalIndex = 1;
     for( auto const &terminal : _terminals )
     {
-        ImVec2 terminalPosition{ terminal->Position.x + 5.0f, terminal->Position.y + 5.0f };
-        ImVec2 terminalSize{ terminal->Size.x - 10.0f, terminal->Size.y - 10.0f };
         ImGui::SetCursorPos( terminal->Position );
         ImGui::PushID( (void *)terminal.get() );
         ImGui::BeginChild( "##ChildItem", terminal->Size );
         {
-            auto  *drawList    = ImGui::GetWindowDrawList();
-            ImVec2 topLeft     = ImGui::GetCursorScreenPos();
-            ImVec2 windowSize  = ImGui::GetWindowSize();
+            auto  *drawList       = ImGui::GetWindowDrawList();
+            ImVec2 screenPosition = ImGui::GetCursorScreenPos();
+            ImVec2 screenSize     = ImGui::GetWindowSize();
+
+            ImVec2 topLeft{ screenPosition.x, screenPosition.y };
+            ImVec2 windowSize{ screenSize.x, screenSize.y };
             ImVec2 bottomRight = ImVec2{ topLeft.x + windowSize.x, topLeft.y + windowSize.y };
 
             uint32_t borderColor = terminal->IsFocused ? ImColor( 255, 255, 255, 150 ) : ImColor( 255, 255, 255, 50 );
             drawList->AddRect( topLeft, bottomRight, borderColor );
 
-            // ImGui::SetCursorPos( ImVec2{ 5.0f, 5.0f } );
-            ImGui::Text( "FOOBAR %d", terminalIndex++ );
-            ImGui::Text( "%f x %f", windowSize.x, windowSize.y );
+            ImVec2 terminalPosition{ terminal->Position.x + 5.0f, terminal->Position.y + 5.0f };
+            ImVec2 terminalSize{ terminal->Size.x - 10.0f, terminal->Size.y - 10.0f };
+            ImGui::SetCursorPos( terminalPosition );
+            ImGui::PushID( terminalIndex );
+            ImGui::BeginChild( "##TerminalDisplay", terminalSize );
+            {
+                ImGui::Text( "FOOBAR %d", terminalIndex++ );
+                ImGui::Text( "%f x %f", terminalSize.x, terminalSize.y );
+            }
+            ImGui::EndChild();
+            ImGui::PopID();
 
             ImVec2 overlayTopLeft     = ImVec2{ topLeft.x + 1, topLeft.y + 1 };
             ImVec2 overlayBottomRight = ImVec2{ bottomRight.x - 1, bottomRight.y - 1 };
