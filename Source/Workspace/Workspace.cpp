6904a7bba453a2ab6f91bc71ef7e8cab98975495
Jean-Martin Albert <jm.albert@gmail.com>
2024-02-27 13:55:45 -0500

    Refactoring

--- Source/Workspace/Workspace.cpp
+++ Source/Workspace/Workspace.cpp
@@ -5,9 +5,9 @@ Workspace::Workspace( string_t name )
     : _name{ name }
 {
     _terminals.push_back( std::make_shared<Terminal>() );
-    _terminalTree = std::make_shared<Tree>( _terminals.back() );
-
     _terminals.back()->IsFocused = true;
+
+    _terminalTree = std::make_shared<Tree>( _terminals.back() );
 }
 
 void Workspace::VSplit()
@@ -25,8 +25,12 @@ void Workspace::HSplit()
 void Workspace::Render()
 {
     _terminalTree->SetSize( ImGui::GetContentRegionAvail() );
+
+    int32_t terminalIndex = 1;
     for( auto const &terminal : _terminals )
     {
+        ImVec2 terminalPosition{ terminal->Position.x + 5.0f, terminal->Position.y + 5.0f };
+        ImVec2 terminalSize{ terminal->Size.x - 10.0f, terminal->Size.y - 10.0f };
         ImGui::SetCursorPos( terminal->Position );
         ImGui::PushID( (void *)terminal.get() );
         ImGui::BeginChild( "##ChildItem", terminal->Size );
@@ -39,22 +43,19 @@ void Workspace::Render()
             uint32_t borderColor = terminal->IsFocused ? ImColor( 255, 255, 255, 150 ) : ImColor( 255, 255, 255, 50 );
             drawList->AddRect( topLeft, bottomRight, borderColor );
 
-            ImGui::SetCursorPos( ImVec2{ 5.0f, 5.0f } );
-            ImGui::Text( "FOOBAR" );
+            // ImGui::SetCursorPos( ImVec2{ 5.0f, 5.0f } );
+            ImGui::Text( "FOOBAR %d", terminalIndex++ );
             ImGui::Text( "%f x %f", windowSize.x, windowSize.y );
 
+            ImVec2 overlayTopLeft     = ImVec2{ topLeft.x + 1, topLeft.y + 1 };
+            ImVec2 overlayBottomRight = ImVec2{ bottomRight.x - 1, bottomRight.y - 1 };
             if( !terminal->IsFocused )
             {
-                ImVec2 overlayTopLeft     = ImVec2{ topLeft.x + 1, topLeft.y + 1 };
-                ImVec2 overlayBottomRight = ImVec2{ bottomRight.x - 1, bottomRight.y - 1 };
-
                 drawList->AddRectFilled( overlayTopLeft, overlayBottomRight, IM_COL32( 0, 0, 0, 200 ) );
             }
             else
             {
-                ImVec2   overlayTopLeft     = ImVec2{ topLeft.x + 1, topLeft.y + 1 };
-                ImVec2   overlayBottomRight = ImVec2{ bottomRight.x - 1, bottomRight.y - 1 };
-                uint32_t borderColor        = ImColor( 255, 55, 255, 200 );
+                uint32_t borderColor = ImColor( 255, 55, 255, 200 );
 
                 drawList->AddRect( overlayTopLeft, overlayBottomRight, borderColor );
             }
