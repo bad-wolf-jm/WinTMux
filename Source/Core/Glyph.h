ba7663806fe4497b57e64a5d89a6c7aa4499d103
Jean-Martin Albert <jm.albert@gmail.com>
2024-03-15 19:58:44 -0400

    Added some missing files

---
+++ Source/Core/Glyph.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "Core/String.h"
+
+enum CharacterAttribute : uint32_t
+{
+    NORMAL        = 0,
+    FAINT         = ( 1 << 0 ),
+    BOLD          = ( 1 << 1 ),
+    ITALIC        = ( 1 << 2 ),
+    UNDERLINE     = ( 1 << 3 ),
+    STRIKETHROUGH = ( 1 << 4 ),
+    REVERSED      = ( 1 << 5 ),
+    HIDDEN        = ( 1 << 6 ),
+    SLOW_BLINK    = ( 1 << 7 ),
+    FAST_BLINK    = ( 1 << 8 ),
+    DEFAULT_BG    = ( 1 << 9 ),
+    DEFAULT_FG    = ( 1 << 10 )
+};
+
+struct Glyph
+{
+    char_t   Character[4]  = { 0 };
+    uint32_t CharacterSize = 1;
+
+    // The attributes member encodes the glyph's character attributes,
+    // as well as the foreground and background color:
+    //   | CharacterAttributes | Foreground  | Background |
+    //   |       16 bits       |   24 bits   |   24 bits  |
+    uint64_t Attributes = 0;
+};
